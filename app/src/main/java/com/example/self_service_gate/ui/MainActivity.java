cd14df47cfd51e4ce990fb3ecfb886750c616bb8
yangchi <yangchi@163.com>
2020-05-06 19:44:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/self_service_gate/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.self_service_gate.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.self_service_gate.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/self_service_gate/ui/SelfServiceGateActivity.java
@@ -0,0 +1,16 @@
+package com.example.self_service_gate.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.self_service_gate.R;
+
+public class SelfServiceGateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_service_gate);
+    }
+}
